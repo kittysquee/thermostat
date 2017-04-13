8ca1dc56f7458b4aa113aa7c6199076f924dafeb
kittysquee <kittysquee@gmail.com>
2017-04-13 09:50:03 +0100

    Adds interaction and styling

--- interface.js
+++ interface.js
@@ -1,5 +1,32 @@
+var thermostat = new Thermostat;
+
+var temperatureDisplay = function() {
+  $("#temperature").text(thermostat.temperature);
+  $("#power-saving-status").text(thermostat.powerUsage());
+  $('#temperature').attr('class', thermostat.powerUsage());
+};
+
+
 $(document).ready(function() {
-  $('#temperature-up').mouseenter(function(){
-    $('#temperature-up').fadeTo('fast', 0);
+  temperatureDisplay();
+  $('#temperature-up').click(function(){
+    thermostat.increase();
+    temperatureDisplay();
+  });
+  $("#temperature-down").click(function(){
+    thermostat.decrease();
+    temperatureDisplay();
+  });
+  $("#temperature-reset").click(function(){
+    thermostat.reset();
+    temperatureDisplay();
+  });
+  $("#powersaving-on").click(function(){
+    thermostat.powerSaveSwitchOn();
+    temperatureDisplay();
+  });
+  $("#powersaving-off").click(function(){
+    thermostat.powerSaveSwitchOff();
+    temperatureDisplay();
   });
 });
